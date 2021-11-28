0317c61eecb111562189072fd11982f7d20c3fae
BabyTigerChaseDream <scarlett_guojia@hotmail.com>
2021-11-28 11:04:41 +0800

    [Optional-mthread] Web Server and Client socket programming-1

--- python/TCPServer.py
+++ python/TCPServer.py
@@ -12,11 +12,11 @@ serverSocket.bind(('',serverPort))
 serverSocket.listen(1)
 print("The server is ready to receive")
 while 1:
-    connectionSocket, addr = serverSocket.accept()
-    sentence = connectionSocket.recv(1024)
-    # first key:value part in connectionSocket.recv(1024) is 'GET/POST':(url path)
+    connSocket, addr = serverSocket.accept()
+    sentence = connSocket.recv(1024)
+    # first key:value part in connSocket.recv(1024) is 'GET/POST':(url path)
     print("[received socket data]\n\t",sentence.split()[1],"#\n")
-    #print("connectionSocket,addr,sentense",connectionSocket,addr,sentence)
+    #print("connSocket,addr,sentense",connSocket,addr,sentence)
     #print("sentense",sentence.split())
 
     # decode bytes into strings
@@ -24,29 +24,36 @@ while 1:
     print('### Type of filename : ',type(filename))
 
     try:
-        header = 'HTTP/1.1 200 OK \nConnection: close\n' + \
+        '''
+        header = 'HTTP/1.1 200 OK \nConnection: keep-alive\n' + \
         'Content0Length: {}\n'.format("To Be Decided") + \
         'Content-Type: text/html\n\n'
-        connectionSocket.send(header.encode())
-        output = 'Thank you for connecting\n'
-        connectionSocket.send(output.encode())
+        connSocket.send(header.encode())
+        '''
+
+        header_default = 'HTTP/1.1 404 {} Not Found\nContent-Type: text/html\n\n'
+
+        #output = 'Thank you for connecting\n'
+        #connSocket.send(output.encode())
 
         os.system('pwd')
         cwd = os.getcwd()
         filename_cwd = cwd + filename
+        reply = 'empty\n\r'
         if os.path.isfile(filename_cwd):
             with open(filename_cwd) as f:
                 reply = f.read()
-            header = 'HTTP/1.1 200 OK \nConnection: close\n' + \
-            'Content0Length: <h4>{}\n<\h4>'.format(filename_cwd) + \
-            'Content-Type: text/html\n\n'
+            #header = 'HTTP/1.1 200 OK \nConnection: keep-alive\n' + \
+            #'Content0Length:<html><body><h4>{}<\h4></body></html>'.format(filename_cwd) + \
+            #'Content-Type: text/html\n\n'
+            header = 'HTTP/1.1 200 OK\nContent-Type: text/html\n\n'
         else:
-            header = 'HTTP/1.1 404 {} Not Found'.format(filename_cwd)
+            header = header_default.format(filename_cwd)
 
-        connectionSocket.send(header.encode())
-        connectionSocket.send(reply.encode())
-        connectionSocket.close()
+        connSocket.send(header.encode())
+        connSocket.send(reply.encode())
+        connSocket.close()
     except IOError:
-        header = 'HTTP/1.1 404 ALL Not Found'
-        connectionSocket.send(header.encode())
-        connectionSocket.close()
+        header = header_default.format('nothing on filename')
+        connSocket.send(header.encode())
+        connSocket.close()

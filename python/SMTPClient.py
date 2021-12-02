c93dc4f1b6d850150e97fc4165cb03d6e181c8a6
BabyTigerChaseDream <scarlett_guojia@hotmail.com>
2021-12-03 07:59:45 +0800

    Working : SMTP client basic flow and class - 163 mail

--- python/SMTPClient.py
+++ python/SMTPClient.py
@@ -5,7 +5,7 @@ import time
 def msgwrapper(msg):
     return msg+'\r\n'
 
-def Email(self):
+class Email:
     def __init__(self, mailserver='smtp.163.com', mailport=25):
         # constant of Email client  
         self.mailserver = mailserver
@@ -18,9 +18,16 @@ def Email(self):
         self.DataMsg = msgwrapper("DATA")
 
         # email info 
+        self.uname = 'codingmylife0925@163.com'
+        self.pwd = 'GLFALBFKQUBTPFQT'
+        # receiver info 
         self.rcptname='codingmylife0925@163.com'
 
-    def infoEncode(self, uname='codingmylife0925@163.com',  pwd='GLFALBFKQUBTPFQT'):
+    def infoEncode(self, uname=None,  pwd=None):
+        if not uname:
+            uname=self.uname
+        if not pwd:
+            pwd= self.pwd
         # user account info
         self.enuname=base64.b64encode(uname.encode()).decode() 
         self.enpwd=base64.b64encode(pwd.encode()).decode() 
@@ -28,28 +35,39 @@ def Email(self):
     def connect(self):
         self.csocket = socket(AF_INET, SOCK_STREAM) 
         self.csocket.connect((self.mailserver,self.mailport)) 
-        recv=clientSocket.recv(1024).decode() 
-        # expect msg below
-        #'220 163.com Anti-spam GT for Coremail System (163com[20141201])\r\n' 
-        print(recv)
+        recv=self.csocket.recv(1024).decode() 
+        if recv[:3] != '220':
+            # expect msg below
+            #'220 163.com Anti-spam GT for Coremail System (163com[20141201])\r\n' 
+            print(recv)
+            raise ValueError('220 reply not received from server.')
 
-    def helo(self, nickname='JiaJia'):
+    def sendhelo(self, nickname='JiaJia'):
         self.heloMsg = self.helo.format(user=nickname)
         # messages' template 
         self.csocket.send(self.heloMsg.encode()) 
         recv=self.csocket.recv(1024).decode() 
+        if recv[:3] != '250':
+            #'250 OK\r\n' 
+            print(recv)
+            raise ValueError('250 reply not received from server.')
         print(recv)
-        #'250 OK\r\n' 
 
     def authlogin(self):
         self.csocket.send(self.authMsg.encode()) 
         recv=self.csocket.recv(1024).decode() 
+        if recv[:3] != '334':
+            #'334 dXNlcm5hbWU6\r\n' 
+            print(recv)
+            raise ValueError('334 reply not received from server.')
         print(recv)
-        #'334 dXNlcm5hbWU6\r\n' 
         self.csocket.sendall((self.enuname + '\r\n').encode()) 
+
         recv=self.csocket.recv(1024).decode() 
-        print(recv)
-        #'334 UGFzc3dvcmQ6\r\n' 
+        if recv[:3] != '334':
+            #'334 UGFzc3dvcmQ6\r\n' 
+            print(recv)
+            raise ValueError('334 reply not received from server.')
         self.csocket.sendall((self.enpwd + '\r\n').encode()) 
         recv=self.csocket.recv(1024).decode() 
         print(recv)
@@ -63,14 +81,14 @@ def Email(self):
         recv=self.csocket.recv(1024).decode() 
         print(recv)
         #'250 Mail OK\r\n' 
-        self.rcptToMsg = self.rcptTo.format(uname=rcptname)
+        self.rcptToMsg = self.rcptTo.format(rname=rcptname)
         self.csocket.send(self.rcptToMsg.encode()) 
         recv=self.csocket.recv(1024).decode() 
         print(recv)
         self.csocket.send(self.DataMsg.encode()) 
-        recv4 = self.csocket.recv(1024) 
-        recv4 = recv4.decode() 
-        print("After DATA command: "+recv4) 
+        recv = self.csocket.recv(1024) 
+        recv = recv.decode() 
+        print("After DATA command: "+recv) 
 
         # start content writing 
         subject = "Subject: testing my client\r\n\r\n" 
@@ -87,9 +105,15 @@ def Email(self):
         endmsg = "\r\n.\r\n" 
         self.csocket.send(endmsg.encode()) 
         recv_msg = self.csocket.recv(1024) 
+        print(recv)
         print("Response after sending message body:"+recv_msg.decode()) 
         #quit = "QUIT\r\n" 
         #self.csocket.send(quit.encode()) 
 
 if __name__ == '__main__':
-    conn()
+    E = Email()
+    E.infoEncode()
+    E.connect()
+    E.sendhelo()
+    E.authlogin()
+    E.sendmail()
